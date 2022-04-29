2d22ff330f5d6cd5165749e9ca2910e09c980579
pc <pc@EDURAZEE>
2022-04-29 09:15:18 +0200

    Add project files.

---
+++ Yendrek's code to CSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yendrek_s_code_to_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
